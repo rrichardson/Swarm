3b0423ee8055bfa45b652a4992aec4f8c5c11d76
Rick <rick@whitey.(none)>
2009-10-26 18:39:34 -0400

    updating fork

---
+++ src/main/scala/swarm/Queen.scala
@@ -0,0 +1,17 @@
+/*
+ * Queen.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package swarm
+
+import scala.collection.mutable.HashSet
+
+class Queen {
+  val UpdateThreshold = 5;
+
+  val peers = new HashSet[Location];
+
+}
